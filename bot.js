00135ad939163d48ab168a4f9ca0956f64918dc4
dddfffss <mazderfazder00@gmail.com>
2025-01-03 21:34:41 +0300

    исправлен продакшн

---
+++ bot.js
@@ -0,0 +1,41 @@
+const express = require("express");
+const axios = require("axios");
+
+const app = express();
+
+// Замените на токен вашего Telegram-бота
+const botToken = "7822839883:AAGpnct2WADp29i4_u5s_txMAoeDxKBSPtc";
+
+// Укажите ваш Telegram chat ID
+const chatId = "1080261812";
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log(`Сервер запущен на порту ${PORT}`);
+});
+
+
+// Middleware для обработки JSON
+app.use(express.json());
+
+// Маршрут для получения данных от вашего сайта
+app.post("/send-data", async (req, res) => {
+    const { message } = req.body; // Получаем сообщение из тела запроса
+    try {
+        // Отправляем сообщение в Telegram
+        await axios.post(`https://api.telegram.org/bot${botToken}/sendMessage`, {
+            chat_id: chatId,
+            text: message,
+        });
+        res.send("Сообщение отправлено в Telegram");
+    } catch (error) {
+        console.error("Ошибка отправки:", error.response.data);
+        res.status(500).send("Ошибка при отправке сообщения");
+    }
+});
+
+// Запуск сервера
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Сервер запущен на порту ${PORT}`);
+});
