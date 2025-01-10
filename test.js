c33fb5339720d1d086850f588a0940d791667b66
dddfffss <mazderfazder00@gmail.com>
2025-01-10 22:17:57 +0300

    Исправлен путь и настройки запуска

--- test.js
+++ test.js
@@ -1,14 +1,24 @@
-const axios = require("axios");
+const axios = require('axios');
 
-const botToken = "7822839883:AAGpnct2WADp29i4_u5s_txMAoeDxKBSPtc";
-const chatId = "1080261812";
-const message = "Тестовое сообщение";
+// Указание токена бота
+const botToken = '7822839883:AAGpnct2WADp29i4_u5s_txMAoeDxKBSPtc';
 
-axios.post(`https://api.telegram.org/bot${botToken}/sendMessage`, {
-    chat_id: chatId,
-    text: message
-}).then(response => {
-    console.log("Сообщение отправлено:", response.data);
-}).catch(error => {
-    console.error("Ошибка отправки:", error.response ? error.response.data : error.message);
-});
+// Указание ID чата
+const chatId = '1080261812';
+
+// Функция для отправки сообщения
+const sendMessage = async (message) => {
+    try {
+        const response = await axios.post(`https://api.telegram.org/bot${botToken}/sendMessage`, {
+            chat_id: chatId,
+            text: message,
+        });
+        console.log('Сообщение успешно отправлено:', response.data);
+    } catch (error) {
+        console.error('Ошибка при отправке сообщения:', error.response ? error.response.data : error.message);
+    }
+};
+
+// Отправляем тестовое сообщение
+sendMessage('Привет! Это тестовое сообщение от Telegram бота.');
+
